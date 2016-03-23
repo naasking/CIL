171d4be7130af5ef1b427671ba764b9497ea968d
Sandro Magi <naasking@gmail.com>
2016-03-23 10:12:47 -0400

    added an xunit test project

---
+++ CIL.Tests/CILTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using Xunit;
+using CIL.Expressions;
+
+namespace CIL.Tests
+{
+    public class CILTests
+    {
+        [Fact]
+        public void TestSimple()
+        {
+            Func<int> foo = () => 3;
+            Expression<Func<int>> fooe = () => 3;
+            Assert.Equal(fooe.ToString(), foo.GetExpression().ToString());
+            Assert.NotEqual(Expression.Constant(3).ToString(), Expression.Constant(0).ToString());
+        }
+    }
+}
