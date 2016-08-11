691b31ef8cba106a8d7f353d582c9ed3325f52c3
Sandro Magi <naasking@gmail.com>
2016-08-11 12:44:20 -0400

    moved nuget packaging into its own folder with build/upload scripts

---
+++ nuget/build.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set vers=0.1.0
+set libs=CIL.NET
+set tools=
+set files=%tools% %libs%
+
+:: clear whole folder
+::for %%i in (%files%) do rd /S /Q "%%i"
+
+:: generate packages
+del *.nupkg
+for %%i in (%files%) do sed -e "s/[$]vers[$]/%vers%/" %%i.nuspec.xml > %%i.nuspec
+for %%i in (%files%) do nuget pack %%i.nuspec
